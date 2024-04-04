e72a69e9d1b2ff8c6ae567f9052bd5376fc75e4a
noushin-nusrat <noushinnusrat21@gmail.com>
2024-04-04 10:14:51 +0600

    added some additional changes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,7 +8,8 @@ const router = createBrowserRouter([
         element: <Root></Root>,
         children: [
             {
-                path:<Home></Home>
+                path:'/',
+                element: <Home></Home>
             }
         ]
     }
